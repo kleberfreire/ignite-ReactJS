a922ff5e4bbb59ad5899be2437c20a6944b8ef0b
Kleber Freire <ch4r4d4@gmail.com>
2022-05-18 21:55:23 -0300

    Modulo I iniciando com o react

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+const isDevelopment = process.env.NODE_ENV !== "production";
 module.exports = {
-  mode: "development",
-  devtool: "eval-source-map",
+  mode: isDevelopment ? "development" : "production",
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     filename: "bundle.js",
@@ -29,6 +30,11 @@ module.exports = {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },

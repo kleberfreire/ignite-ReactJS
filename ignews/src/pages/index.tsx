957aa0335db738b45aacfcb833fc2c7b95296cbe
Kleber Freire <ch4r4d4@gmail.com>
2022-05-28 21:35:09 -0300

    chapter III - iniciado

---
+++ ignews/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Header } from "../components/Header";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1>Hello world!</h1>
+    </>
+  );
+};
+
+export default Home;

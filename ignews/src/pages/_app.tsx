18c839871609d48370eb82617d188f175475474b
Kleber Freire <ch4r4d4@gmail.com>
2022-05-30 16:17:51 -0300

    Chapter III - configurando o prismic

--- ignews/src/pages/_app.tsx
+++ ignews/src/pages/_app.tsx
@@ -1,7 +1,6 @@
+import "../styles/global.scss";
 import type { AppProps } from "next/app";
 import { Header } from "../components/Header";
-
-import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,9 +1,17 @@
-import type { NextPage } from "next";
+import type { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
 
-const Home: NextPage = () => {
+interface IHomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: IHomeProps) {
   return (
     <>
       <Head>
@@ -18,16 +26,38 @@ const Home: NextPage = () => {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90</span>
+            <span>for {product.amount}</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
-};
+}
 
-export default Home;
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1L4mlYCcyPsjkE3qLezJQfKK", {
+    expand: ["product"],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: price.unit_amount,
+  };
+
+  return {
+    props: {
+      product: {
+        priceId: price.id,
+        amount: Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(price.unit_amount ? price.unit_amount / 100 : 0),
+      },
+    },
+    revalidate: 60 * 60 * 24, //24 hours
+  };
+};
